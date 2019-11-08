d3b329a7964ea475a915430f1101b9654cd36009
mnmpower <mnmmaarten@hotmail.com>
2019-11-08 11:32:25 +0100

    removedswagger after mongo DB lergch conflict

--- src/main/java/com/thomasmore/RaceService/Entity/Race.java
+++ src/main/java/com/thomasmore/RaceService/Entity/Race.java
@@ -1,28 +1,20 @@
 package com.thomasmore.RaceService.Entity;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import javax.persistence.*;
 
-@Entity
-@Table(name="Race")
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+
 @Data
-@NoArgsConstructor
+@Document(collection = "Race")
+
 public class Race {
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-    @Column(name="id")
-    private int id;
-
-    @Column(name="name")
+    private String id;
     private String name;
-
-    @Column(name="speed")
     private String speed;
-    @Column(name="alignment")
     private String alignment;
-    @Column(name="size")
     private String size;
-
-    @Column(name="abilityBonuses")
     private String abilityBonuses;
 }
--- src/main/java/com/thomasmore/RaceService/Repository/RaceRepository.java
+++ src/main/java/com/thomasmore/RaceService/Repository/RaceRepository.java
@@ -1,11 +1,11 @@
 package com.thomasmore.RaceService.Repository;
 
 import com.thomasmore.RaceService.Entity.Race;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin
-public interface RaceRepository extends JpaRepository<Race, Integer> {
-    Race findRaceById(@Param("id") int id);
+public interface RaceRepository extends MongoRepository<Race, String> {
+//    Race findRaceById(@Param("id") String id);
 }
