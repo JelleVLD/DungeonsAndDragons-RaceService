601f91d3776bdd7024b5c42f6c0bccfe9e77171d
mnmpower <mnmmaarten@hotmail.com>
2019-11-21 14:02:36 +0100

    added findRaceById

--- src/main/java/com/thomasmore/RaceService/Repository/RaceRepository.java
+++ src/main/java/com/thomasmore/RaceService/Repository/RaceRepository.java
@@ -1,13 +1,8 @@
 package com.thomasmore.RaceService.Repository;
 
-import com.sun.org.apache.xpath.internal.objects.XNull;
 import com.thomasmore.RaceService.Entity.Race;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.web.SortDefault;
-import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
@@ -19,9 +14,11 @@ public interface RaceRepository extends MongoRepository<Race, String> {
     List<Race> findRacesBySpeed(@Param("speed") Integer speed);
     List<Race> findRacesBySize(@Param("size") String size);
     Race findRacesByName(@Param("name") String name);
+    Race findRacesById(@Param("id") String id);
 
 }
 
 // http://localhost:8001/races/search/findRacesBySpeed?speed=30
 // http://localhost:8001/races/search/findRacesBySize?size=medium
 // http://localhost:8001/races/search/findRacesByName?name=Dwarf
+// http://localhost:8001/races/search/findRacesById?id=5dd6737d1c9d4400008a5a40
